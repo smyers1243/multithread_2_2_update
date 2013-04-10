b0ed76c5dea6a86a72623cb7fbbc16c91ab11392
scott myers <smyers@catalystitservices.com>
2013-04-10 11:56:14 -0700

    uncommented code

--- runSh.sh
+++ runSh.sh
@@ -24,17 +24,17 @@ perl data_update_driver.pl --dir /home/kclsdev/scripts/04_record_entry_three --f
 echo 05_a 
 date
 perl data_update_driver.pl --dir /home/kclsdev/scripts/05_a_record_entry_four --finished-dir /home/kclsdev/scripts/05_a_record_entry_four/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen --exit-on-error
-#echo 05_b 
-#date
-#perl update_driver.pl --dir /home/kclsdev/scripts/05_b_record_entry_four/before --finished-dir /home/kclsdev/scripts/05_b_record_entry_four/before/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
-#perl data_update_driver.pl --dir /home/kclsdev/scripts/05_b_record_entry_four --finished-dir /home/kclsdev/scripts/05_b_record_entry_four/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
-#perl update_driver.pl --dir /home/kclsdev/scripts/05_b_record_entry_four/after --finished-dir /home/kclsdev/scripts/05_b_record_entry_four/after/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
+echo 05_b 
+date
+perl update_driver.pl --dir /home/kclsdev/scripts/05_b_record_entry_four/before --finished-dir /home/kclsdev/scripts/05_b_record_entry_four/before/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
+perl data_update_driver.pl --dir /home/kclsdev/scripts/05_b_record_entry_four --finished-dir /home/kclsdev/scripts/05_b_record_entry_four/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
+perl update_driver.pl --dir /home/kclsdev/scripts/05_b_record_entry_four/after --finished-dir /home/kclsdev/scripts/05_b_record_entry_four/after/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
 echo 05_c 
 date
 perl data_update_driver.pl --dir /home/kclsdev/scripts/05_c_record_entry_four --finished-dir /home/kclsdev/scripts/05_c_record_entry_four/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen --exit-on-error
-#echo 06 
-#date
-#perl data_update_driver.pl --dir /home/kclsdev/scripts/06_record_entry_b_maintain --finished-dir /home/kclsdev/scripts/06_record_entry_b_maintain/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
+echo 06 
+date
+perl data_update_driver.pl --dir /home/kclsdev/scripts/06_record_entry_b_maintain --finished-dir /home/kclsdev/scripts/06_record_entry_b_maintain/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen 
 echo 07 
 date
 perl data_update_driver.pl --dir /home/kclsdev/scripts/07_record_entry_bbb --finished-dir /home/kclsdev/scripts/07_record_entry_bbb/fin --database evergreen_test --host 192.168.0.215 --user evergreen --password evergreen --exit-on-error
